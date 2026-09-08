4d1e444a1d51e203c3fbf5ac34146f0548fcc1f7
Divya0912 <divya050202@gmail.com>
2026-09-08 16:56:10 +0800

    Implement Hero Scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,18 @@
 import gsap from 'gsap';
 import {useGSAP} from '@gsap/react'
-import {SplitText} from "gsap/all";
+import {ScrollTrigger, SplitText} from "gsap/all";
+import {useRef} from "react";
+import {useMediaQuery} from "react-responsive";
+
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
 const Hero = () => {
+    const videoRef = useRef(null);
+
+    const isMobile = useMediaQuery({maxWidth:767})
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title',{type:'chars,words'});
         const paragraphSplit = new SplitText('.subtitle',{type:'lines'});
@@ -29,50 +38,86 @@ const Hero = () => {
         gsap.timeline({
             scrollTrigger:{
                 trigger:'#hero',
-                start: 'to top',
-                end: 'bottom top',
-                scrub: true,
+                start:'top top',
+                end:'bottom top',
+                scrub:true,
             }
         })
             .to('.right-leaf',{y:200},0)
-            .to('.left-leaf',{y:-200},0)
+            .to('.left-leaf',{y:-200},0);
+
+        const startValue = isMobile ? 'top 50%': 'center 60%';
+        const endValue = isMobile ? '120% top': 'bottom top';
+
+        // Video animation timeline
+        //Create the timeline with a default duration
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current,{
+                currentTime:videoRef.current.duration
+            })
+        }
     },[]);
+
     return(
-       <>
-           <section id="hero" className="noisy">
-               <h1 className="title">MOJITO</h1>
+        <>
+            <section id="hero" className="noisy">
+                <h1 className="title">MOJITO</h1>
 
-               <img
-                   src="/images/hero-left-leaf.png"
-                   alt="left-leaf"
-                   className="left-leaf"
-                   />
+                <img
+                    src="/images/hero-left-leaf.png"
+                    alt="left-leaf"
+                    className="left-leaf"
+                />
 
-               <img
-                   src="/images/hero-right-leaf.png"
-                   alt="right-leaf"
-                   className="right-leaf"
-               />
-               <div className="body">
-                   <div className="content">
-                       <div className="space-y-5 hidden md:block">
-                           <p>Cool. Crisp. Classic.</p>
-                           <p className="subtitle">
-                               Sip the Spirit <br/> of Summer
-                           </p>
-                       </div>
+                <img
+                    src="/images/hero-right-leaf.png"
+                    alt="right-leaf"
+                    className="right-leaf"
+                />
 
-                       <div className="view-cocktails">
-                        <p className="subtitle">
-                          Every cock tails on your menu is a blend of premium ingredients,creative
-                           flair,and timeless recipes - design your senses.
-                        </p>
-                           <a href="#cocktails">View Cocktails</a>
-                       </div>
-                   </div>
-               </div>
-           </section>
-       </>
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Classic.</p>
+
+                            <p className="subtitle">
+                                Sip the Spirit <br/> of Summer
+                            </p>
+                        </div>
+
+                        <div className="view-cocktails">
+                            <p className="subtitle">
+                                Every cock tails on your menu is a blend of premium ingredients,creative
+                                flair,and timeless recipes - design your senses.
+                            </p>
+
+                            <a href="#cocktails">View Cocktails</a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    muted
+                    playsInline
+                    preload="auto"
+                    src="/videos/output.mp4"
+                />
+
+            </div>
+        </>
     )
 }
 
