ff5129d07418bb0c111f6ac568db4155f40952ec
Divya0912 <divya050202@gmail.com>
2026-09-09 19:37:26 +0800

    Implement the Art section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,19 +4,19 @@ import gsap from 'gsap';
 
 const Navbar = () => {
     useGSAP(() => {
-          const navTween = gsap.timeline({
-              scrollTrigger:{
-                  trigger: 'nav',
-                  start: 'bottom top'
-              }
-          });
-          navTween.fromTo('nav', {background:'transparent'},{
-              backgroundColor:'#00000050',
-                  backgroundFilter:'blur(10px)',
-                duration:1,
-                ease: 'power1,inout'
+        const navTween = gsap.timeline({
+            scrollTrigger:{
+                trigger: 'nav',
+                start: 'bottom top'
+            }
         });
-        })
+        navTween.fromTo('nav', {background:'transparent'},{
+            backgroundColor:'#00000050',
+            backdropFilter:'blur(10px)',
+            duration:1,
+            ease: 'power1.inOut'
+        });
+    })
     return (
         <nav>
             <div>
